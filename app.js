758f53dd25c3c12951be8cd0a05c70d9fce3eb01
echizenyayota <virtualmisso30@gmail.com>
2023-11-30 16:56:39 +0900

    Describe the getMessage function

---
+++ app.js
@@ -0,0 +1,12 @@
+const submitButton = document.querySelector('#submit');
+
+async function getMessage() {
+  console.log('clicked');
+  try {
+
+  } catch {
+
+  }
+}
+
+submitButton.addEventListener('click', getMessage);
