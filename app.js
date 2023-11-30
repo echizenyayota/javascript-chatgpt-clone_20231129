f73487beea0d7c0f7a2813c0bcb73cb271eb89b0
echizenyayota <virtualmisso30@gmail.com>
2023-11-30 18:40:36 +0900

    Fetch the API data and show it on the cosole

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+// Please don't show your API Key on the internet 
+const API_KEY = OPENAI_API_KEYNUMBER;
+
 const submitButton = document.querySelector('#submit');
 
 async function getMessage() {
   console.log('clicked');
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user",content: "Hello! How are you?"}],
+      max_tokens: 100,
+    })
+  };
+
   try {
-
-  } catch {
-
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    console.log(data);
+  } catch(error) {
+    console.log(error);
   }
 }
 
